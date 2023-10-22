cbe7b6a6e0bc7d8598a731eb5f249a1f82283a59
a7mad1112 <ahmalawneh79@gmail.com>
2023-10-23 00:05:39 +0300

    Add animation for hamburger btn

--- build/js/script.js
+++ build/js/script.js
@@ -3,7 +3,8 @@ const initApp = () => {
   const mobileMenu = document.getElementById("mobile-menu");
   const toggleMenu = () => {
     mobileMenu.classList.toggle("hidden");
-    mobileMenu.classList.toggle("flex");
+    mobileMenu.classList.toggle("flex"); 
+    hamburgerBtn.classList.toggle('toggle-btn');
   }
   hamburgerBtn.addEventListener('click', toggleMenu);
   mobileMenu.addEventListener('click', toggleMenu);
